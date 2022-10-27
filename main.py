5539caa123541d2b5a590bacaa962f27459bf0d8
phoebusyip <110261461+phoebusyip@users.noreply.github.com>
2022-10-27 10:14:21 -0400

    support for HTML parsing

--- main.py
+++ main.py
@@ -1,33 +1,91 @@
+
+
+from pickletools import pylist
 from selenium import webdriver
-import time
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import csv
+import mysql.connector as msql
+from mysql.connector import Error
+import pandas as pd
 
-# Main Function
-if __name__ == '__main__':
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument('--log-level=3')
+DRIVER_PATH = 'C:/Users/chy32/webscrape/chromedriver'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://tokboard.com/')
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
-    driver.get("https://www.google.com")
+soup = BeautifulSoup(driver.page_source, "html.parser")
+py_list = []
+# song_list = soup.find(class_= "jsx-2291293095")
+songs_list = soup.select('.title')
+# print(type(songs_list))
+# print(songs_list)
+for song in songs_list:
+    add = [song.get_text(), "temp artist"]
+    # print(EachPart.get_text())
+    # add.append(song)
+    py_list.append(add)
 
-    driver.set_window_size(1920, 1080)
+# print(songs_list)
+# opening the csv file in 'w' mode
+file = open('db.csv', 'w', newline ='')
+ 
+with file:
+    # identifying header 
+    header = ['Title', 'Artist']
+    writer = csv.DictWriter(file, fieldnames = header)
+    # print(songs_list)
+    for title, artist in py_list:
+        writer.writerow({'Title': title, 'Artist': 'artist'})
 
-    # Send a get request to the url
-    # driver.get('https://tokboard.com/')
-    driver.get('https://www.indeed.com/q-data-scientist-jobs.html?vjk=f65f08b247368ab8&advn=7494582439330530')
-    # test = driver.find_elements(By.ID, "__next")
-    test = driver.find_elements(By.CLASS_NAME, "jobsearch-LeftPane")
 
-    # for e in
+try:
+    conn = msql.connect(host='localhost', user='root',  
+                        password='mysql')
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE irisDB")
+        print("irisDB database is created")
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+irisData = pd.read_csv("./db.csv")
+# print(irisData.to_string())
+irisData.head()
+cursor = None
+
+try:
+    conn = msql.connect(host='localhost', 
+                           database='irisData', user='root', 
+                           password='mysql')
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+        cursor.execute('DROP TABLE IF EXISTS iris;')
+        print('Creating table....')
+        cursor.execute("CREATE TABLE iris (Title varchar(255) NOT NULL, Artist varchar(255)")
+
+        print("iris table is created....")
+
+        for i,row in irisData.iterrows():
+            sql = "INSERT INTO irisdb.iris VALUES (%s, %s)"
+            cursor.execute(sql, tuple(row))
+            print("Record inserted")
+            # the connection is not autocommitted by default, so we 
+            # must commit to save our changes
+            conn.commit()
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+sql = "SELECT * FROM iris"
+# cursor.execute(sql)
+
+# # Fetch all the records
+# result = cursor.fetchall()
+# for i in result:
+#     print(i)
 
 
-    ret = []
-    time.sleep(2)
-    driver.quit()
+driver.quit()
 
-    print(test)
-    print("Done")
+
