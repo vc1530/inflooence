3df7002c090e8c55e7aa1784e9bd0a0efb618e45
phoebusyip <110261461+phoebusyip@users.noreply.github.com>
2022-10-27 11:21:16 -0400

    working song/artist query and mySQL database

--- main.py
+++ main.py
@@ -2,27 +2,35 @@
 
 from pickletools import pylist
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
 from bs4 import BeautifulSoup
 import csv
 import mysql.connector as msql
 from mysql.connector import Error
 import pandas as pd
 
-DRIVER_PATH = 'C:/Users/chy32/webscrape/chromedriver'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+
+browser_options = Options()
+browser_options.headless = True
+DRIVER_PATH = "./chromedriver"
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = browser_options)
+
 driver.get('https://tokboard.com/')
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
 py_list = []
-# song_list = soup.find(class_= "jsx-2291293095")
 songs_list = soup.select('.title')
-# print(type(songs_list))
-# print(songs_list)
+print(songs_list)
+
+
 for song in songs_list:
     add = [song.get_text(), "temp artist"]
-    # print(EachPart.get_text())
-    # add.append(song)
     py_list.append(add)
+print(songs_list)
 
 # print(songs_list)
 # opening the csv file in 'w' mode
@@ -32,30 +40,29 @@ with file:
     # identifying header 
     header = ['Title', 'Artist']
     writer = csv.DictWriter(file, fieldnames = header)
-    # print(songs_list)
     for title, artist in py_list:
         writer.writerow({'Title': title, 'Artist': 'artist'})
 
 
 try:
-    conn = msql.connect(host='localhost', user='root',  
-                        password='mysql')
+    conn = msql.connect(host='localhost', user='root',
+                        password='mysql123')
     if conn.is_connected():
         cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE irisDB")
-        print("irisDB database is created")
+        cursor.execute("CREATE DATABASE inflooencedb")
+        print("inflooence database is created")
 except Error as e:
     print("Error while connecting to MySQL", e)
 
-irisData = pd.read_csv("./db.csv")
+irisData = pd.read_csv("db.csv")
 # print(irisData.to_string())
 irisData.head()
 cursor = None
 
 try:
-    conn = msql.connect(host='localhost', 
-                           database='irisData', user='root', 
-                           password='mysql')
+    conn = msql.connect(host='localhost',
+                           database='inflooencedb', user='root',
+                           password='mysql123')
     if conn.is_connected():
         cursor = conn.cursor()
         cursor.execute("select database();")
@@ -63,28 +70,27 @@ try:
         print("You're connected to database: ", record)
         cursor.execute('DROP TABLE IF EXISTS iris;')
         print('Creating table....')
-        cursor.execute("CREATE TABLE iris (Title varchar(255) NOT NULL, Artist varchar(255)")
+        cursor.execute("CREATE TABLE songs (Title varchar(255) NOT NULL, Artist varchar(255) NOT NULL)")
 
         print("iris table is created....")
 
         for i,row in irisData.iterrows():
-            sql = "INSERT INTO irisdb.iris VALUES (%s, %s)"
+            sql = "INSERT INTO songs VALUES (%s, %s)"
             cursor.execute(sql, tuple(row))
             print("Record inserted")
-            # the connection is not autocommitted by default, so we 
+            # the connection is not autocommitted by default, so we
             # must commit to save our changes
             conn.commit()
 except Error as e:
     print("Error while connecting to MySQL", e)
 
-sql = "SELECT * FROM iris"
-# cursor.execute(sql)
-
-# # Fetch all the records
-# result = cursor.fetchall()
-# for i in result:
-#     print(i)
-
+# Execute query
+sql = "SELECT * FROM inflooencedb.songs"
+cursor.execute(sql)
+# Fetch all the records
+result = cursor.fetchall()
+for i in result:
+    print(i)
 
 driver.quit()
 
