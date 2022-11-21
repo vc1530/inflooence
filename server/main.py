b301c31953c9532062d96107f1d79408b2dd3c92
phoebusyip <110261461+phoebusyip@users.noreply.github.com>
2022-11-21 14:34:54 -0500

    testing limitless scroll on webscraping

--- server/main.py
+++ server/main.py
@@ -23,6 +23,8 @@ from pymongo import MongoClient, InsertOne
 from fastapi import FastAPI
 from dotenv import dotenv_values
 from routes import router as book_router
+
+import time
 # from dotenv import load_dotenv
 # load_dotenv()
 # import os
@@ -90,6 +92,23 @@ def shutdown_db_client():
 # artist_list = soup.select('.artist')
 # list_songs_obj = []
 
+# limitless scroll
+# time.sleep(2)  # Allow 2 seconds for the web page to open
+# scroll_pause_time = 1 # 
+# screen_height = driver.execute_script("return window.screen.height;")   # get the screen height of the web
+# i = 1
+
+# while True:
+#     # scroll one screen height each time
+#     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+#     i += 1
+#     time.sleep(scroll_pause_time)
+#     # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+#     scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+#     # Break the loop when the height we need to scroll to is larger than the total scroll height
+#     if (screen_height) * i > scroll_height:
+#         break
+# end limitless scroll
 
 # # get spotify wrapper
 # sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
