b2911183d0218f461feba64b1f884eff6d935b0f
phoebusyip <110261461+phoebusyip@users.noreply.github.com>
2022-11-25 06:25:18 -0500

    log

--- server/app.js
+++ server/app.js
@@ -28,6 +28,10 @@ childPython.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
 });
 
+childPython.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
 childPython.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
 });
