c5e01d12a28b8fd592b4214f24d70f10c793426d
phoebusyip <110261461+phoebusyip@users.noreply.github.com>
2022-12-08 04:30:37 -0500

    attempt to fix

--- front-end/src/song/SongCard.js
+++ front-end/src/song/SongCard.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios' 
 import { useNavigate } from 'react-router-dom'
 import SongMessage from './SongMessage' 
-import { PromiseProvider } from 'mongoose'
+// import { PromiseProvider } from 'mongoose'
 
 export default function SongCard ({rank, song, savedSongs, loggedIn}) { 
 
